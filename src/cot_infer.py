0abf770372a30fabf0697889cf50e2fc7f725e27
Jonathan Adam Rico <51609531+jonathanadamrico@users.noreply.github.com>
2022-08-22 18:02:06 +0800

    Add cot_infer.py

---
+++ src/cot_infer.py
@@ -0,0 +1,108 @@
+pip install -q ../dependencies/loguru-lib-ds/loguru-0.5.3-py3-none-any.whl
+pip install -q ../dependencies/bbox-lib-ds
+
+
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import glob
+import shutil
+from ensemble_boxes import *
+import torch
+from PIL import Image
+import tempfile
+import zipfile
+
+# check https://github.com/awsaf49/bbox for source code of following utility functions
+from bbox.utils import coco2yolo, coco2voc, voc2yolo, voc2coco
+from bbox.utils import draw_bboxes, load_image
+from bbox.utils import clip_bbox, str2annot, annot2str
+
+def get_bbox(annots):
+    bboxes = [list(annot.values()) for annot in annots]
+    return bboxes
+
+def get_imgsize(row):
+    row['width'], row['height'] = imagesize.get(row['image_path'])
+    return row
+
+np.random.seed(32)
+colors = [(np.random.randint(255), np.random.randint(255), np.random.randint(255))\
+          for idx in range(1)]
+          
+          
+
+
+# YOLOv5
+!mkdir -p /root/.config/Ultralytics
+
+
+
+def load_model(ckpt_path, conf=0.25, iou=0.50):
+    model = torch.hub.load('../yolov5',
+                           'custom',
+                           path=ckpt_path,
+                           source='local',
+                           force_reload=True)  # local repo
+    model.conf = conf  # NMS confidence threshold
+    model.iou  = iou  # NMS IoU threshold
+    model.classes = None   # (optional list) filter by class, i.e. = [0, 15, 16] for persons, cats and dogs
+    model.multi_label = False  # NMS multiple labels per box
+    model.max_det = 1000  # maximum number of detections per image
+    return model
+    
+    
+def predict(model, img, size=768, augment=False):
+    height, width = img.shape[:2]
+    results = model(img, size=size, augment=augment)  # custom inference size
+    preds   = results.pandas().xyxy[0]
+    bboxes  = preds[['xmin','ymin','xmax','ymax']].values
+    if len(bboxes):
+        bboxes  = voc2coco(bboxes,height,width).astype(int)
+        confs   = preds.confidence.values
+        return bboxes, confs
+    else:
+        return [],[]
+    
+def format_prediction(bboxes, confs):
+    annot = ''
+    if len(bboxes)>0:
+        for idx in range(len(bboxes)):
+            xmin, ymin, w, h = bboxes[idx]
+            conf             = confs[idx]
+            annot += f'{conf} {xmin} {ymin} {w} {h}'
+            annot +=' '
+        annot = annot.strip(' ')
+    return annot
+
+def show_img(img, bboxes, bbox_format='yolo'):
+    names  = ['starfish']*len(bboxes)
+    labels = [0]*len(bboxes)
+    img    = draw_bboxes(img = img,
+                           bboxes = bboxes, 
+                           classes = names,
+                           class_ids = labels,
+                           class_name = True, 
+                           colors = colors, 
+                           bbox_format = bbox_format,
+                           line_thickness = 2)
+    return Image.fromarray(img).resize((800, 400))
+     
+# Might need to extract zip file    
+CKPT_PATH = '../models/model_1.pt'
+CONF = 0.45
+IOU = 0.50
+IMG_SIZE = 6400
+AUGMENT = True
+
+model = load_model(CKPT_PATH, conf=CONF, iou=IOU)
+image_paths = df[df.num_bbox>1].sample(100).image_path.tolist()
+for idx, path in enumerate(image_paths):
+    img = cv2.imread(path)[...,::-1]
+    bboxes, confis = predict(model, img, size=IMG_SIZE, augment=AUGMENT)
+    display(show_img(img, bboxes, bbox_format='coco'))
+    if idx>5:
+        break
+        
